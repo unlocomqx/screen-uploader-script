31c3a69c8eac182993491c9b83a4b0d8f265374c
unlocomqx <amxmodx.unloco@gmail.com>
2017-01-31 20:47:10 +0100

    Implement jpg upload

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+$base_url = 'http://sv.net/dev/screenshot/uploads/';
+
+$file = $_FILES["file"];
+
+if ((int)$file['error']) {
+    exit('Error: an error occured');
+}
+
+$info = getimagesize($file['tmp_name']);
+$type = $info['mime'];
+
+if ($type != 'image/jpeg') {
+    exit('Error: invalid file type');
+}
+
+$filename = uniqid() . '.jpeg';
+if (move_uploaded_file($file['tmp_name'], './uploads/' . $filename)) {
+    exit($base_url . $filename);
+} else {
+    exit('Error: an error occured while copying the upload file');
+}
